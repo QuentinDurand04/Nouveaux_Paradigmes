c2c661f96502f763906b60776588d7a548f628e8
Vincent GEORGES <vincent.georges3@etu.univ-lorraine.fr>
2024-11-12 16:12:01 +0100

    a

--- src/model/Model.php
+++ src/model/Model.php
@@ -1,9 +1,24 @@
 <?php
+
 namespace iutnc\hellokant\model;
 
-class Model {
+class Model
+{
     protected static $table;
     protected static $idColumn = 'id';
+
     protected $atts = [];
 
+    public function __construct(array $t = null){
+        if (!is_null($t)) $this->_atts = $t;
+    }
+
+    public function __get(string $name): mixed{
+        if (array_key_exists($name, $this->_atts))
+            return $this->_atts[$name];
+    }
+
+    public function __set(string $name, mixed $val): void{
+        $this->atts[$name] = $val;
+    }
 }
