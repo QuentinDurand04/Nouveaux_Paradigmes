78d8219de918012e8e679a5f05e9547a25a799b6
Vincent GEORGES <vincent.georges3@etu.univ-lorraine.fr>
2024-11-12 15:58:43 +0100

    squelette

---
+++ src/model/Model.php
@@ -0,0 +1,9 @@
+<?php
+namespace iutnc\hellokant\model;
+
+class Model {
+    protected static $table;
+    protected static $idColumn = 'id';
+    protected $atts = [];
+
+}
---
+++ src/querry/Querry.php
@@ -0,0 +1,6 @@
+<?php
+namespace iutnc\hellokant\querry;
+
+class Querry{
+
+}
