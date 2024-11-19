ffdc81117d6dd022e56c04a53ea31f4a3379e64f
Vincent GEORGES <vincent.georges3@etu.univ-lorraine.fr>
2024-11-19 16:02:06 +0100

    correction par rapport au prof

--- src/model/Model.php
+++ src/model/Model.php
@@ -12,33 +12,34 @@ class Model{
     protected $atts = [];
 
     public function __construct(array $t = null){
-        if (!is_null($t)) $this->_atts = $t;
+        if (!is_null($t)) static::$atts = $t;
     }
 
     public function __get(string $name): mixed{
-        if (array_key_exists($name, $this->_atts))
-            return $this->_atts[$name];
+        if (array_key_exists($name, static::$atts))
+            return static::$atts[$name];
         return null;
     }
 
     public function __set(string $name, mixed $val): void{
-        $this->atts[$name] = $val;
+        static::$atts[$name] = $val;
     }
 
     public function delete(){
-        if ($this->atts[static::$idColumn] === null){
+        if (static::$atts[static::$idColumn] === null){
             throw new HellokantException();
         }
 
         return Query::table(static::$table)
-            ->where(static::$idColumn, '=', $this->atts[static::$idColumn])
+            ->where(static::$idColumn, '=', static::$atts[static::$idColumn])
             ->delete();
     }
 
     public function insert(){
         $id = Query::table(static::$table)
-        ->insert($this->atts);
-        $this->atts['id'] = $id;
+        ->where(static::$idColumn, '=', static::$atts[static::$idColumn])
+        ->insert(static::$atts);
+        static::$atts['id'] = $id;
     }
 
     public static function all() : array {
@@ -50,35 +51,34 @@ class Model{
         return $return;
     }
 
-    public function find(int $id) : array {
+    public function find($t) : array {
         //1ère méthode
+        /*
         $m = Query::table(static::$table)
             ->where(static::$idColumn, '=', $id)
             ->get();
         return $m;
+        */
 
         //2ème méthode
-        /*
         $colone = $t[0];
         $operateur = $t[1];
         $valeur = $t[2];
         $m = Query::table(static::$table)
             ->where($colone, $operateur, $valeur)
             ->get();
-        */
+        return $m;
     }
 
     public function belongs_to($table, $cle){
-        $m = Query::table($this->table + ", " + $table)
-            ->where($this->table + "." + $this->idColumn, "=", $table + "." + $cle)
-            ->get();
-        return new static($m);
+        $find = $table::find($this->$cle);
+        return $find[0];
     }
 
     public function has_many($table, $cle){
         $t = [];
-        $m = Query::table($table + ", " + $this->table) 
-            ->where($table + "." + $cle, "=", $this->table + "." + $this->idColumn)
+        $m = Query::table($table + ", " + static::$table) 
+            ->where($table + "." + $cle, "=", static::$table + "." + static::$idColumn)
             ->get();
         foreach($m as $elem){
             $t[] = new static($elem);
